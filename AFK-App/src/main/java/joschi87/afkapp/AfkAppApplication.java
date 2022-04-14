3f35bf277febae5b3a5e5e2a139e30339b14df92
Joschi87 <janosch.walter27@gmail.com>
2022-04-14 23:33:31 +0200

    Inital Project Commit for AFK-APP

---
+++ AFK-App/src/main/java/joschi87/afkapp/AfkAppApplication.java
@@ -0,0 +1,13 @@
+package joschi87.afkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfkAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfkAppApplication.class, args);
+    }
+
+}
