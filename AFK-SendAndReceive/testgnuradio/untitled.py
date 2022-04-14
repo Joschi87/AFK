7b1a29d25ec841f5f749e8c8783104d02699f041
Achim <achimdahlhoff@gmx.de>
2022-04-15 00:42:56 +0200

    add testgnuradio

---
+++ AFK-SendAndReceive/testgnuradio/untitled.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: ping
+# Author: adahlhoff
+# GNU Radio version: 3.10.1.1
+
+from gnuradio import blocks
+import pmt
+from gnuradio import gr
+from gnuradio.filter import firdes
+from gnuradio.fft import window
+import sys
+import signal
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+from gnuradio import zeromq
+
+
+
+
+class untitled(gr.top_block):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "ping", catch_exceptions=True)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 32000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.zeromq_pub_msg_sink_0 = zeromq.pub_msg_sink('tcp://127.0.0.1:5556', 250, True)
+        self.blocks_message_strobe_0 = blocks.message_strobe(pmt.intern("CLICK"), 2500)
+        self.blocks_message_debug_0 = blocks.message_debug(True)
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.blocks_message_debug_0, 'print'))
+        self.msg_connect((self.blocks_message_strobe_0, 'strobe'), (self.zeromq_pub_msg_sink_0, 'in'))
+
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+
+
+
+def main(top_block_cls=untitled, options=None):
+    tb = top_block_cls()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    tb.start()
+
+    tb.wait()
+
+
+if __name__ == '__main__':
+    main()
