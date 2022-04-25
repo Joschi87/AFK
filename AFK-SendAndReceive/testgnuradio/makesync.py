453c8d40f271fdcc54fd6b22b2713403959dd280
Achim <mail_achim@gmx.de>
2022-04-25 15:13:31 +0200

    fix up a rough gnuradio ofdm transmitter.
    Finally, some output. Rough first steps for OFDM in GnuRadio.

--- AFK-SendAndReceive/testgnuradio/makesync.py
+++ AFK-SendAndReceive/testgnuradio/makesync.py
@@ -6,7 +6,15 @@ import random
 
 fft_len = 128
 
-v = list(random.random()*2.0-1.0 for i in range(fft_len))
+RMIN = 3
+RMAX = 20
+
+v = list()
+for i in range(fft_len):
+ val = 0.0
+ if i>=RMIN and i<=RMAX:
+  val = random.random()*2.0-1.0
+ v.append(val)
 vs = list("%.2f"%(b,) for b in v)
 vs = list("0j+"+b for b in vs)
 print("("+",".join(vs)+")")
--- AFK-SendAndReceive/testgnuradio/ofdm_enc2.py
+++ AFK-SendAndReceive/testgnuradio/ofdm_enc2.py
@@ -21,15 +21,19 @@ if __name__ == '__main__':
         except:
             print("Warning: failed to XInitThreads()")
 
+from PyQt5 import Qt
+from gnuradio import qtgui
+from gnuradio.filter import firdes
+import sip
 from gnuradio import blocks
 import numpy
 from gnuradio import digital
-from gnuradio import gr
-from gnuradio.filter import firdes
+from gnuradio import fft
 from gnuradio.fft import window
+from gnuradio import filter
+from gnuradio import gr
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
@@ -74,32 +78,100 @@ class ofdm_enc2(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 32000
+        self.samp_rate = samp_rate = 24000
         self.fft_len = fft_len = 128
 
         ##################################################
         # Blocks
         ##################################################
-        self.digital_ofdm_carrier_allocator_cvc_0 = digital.ofdm_carrier_allocator_cvc( fft_len, ((4,5,6,7,9,10,11,12,14,15),), ((3,8,13),), ((1+0j,1+0j,1+0j),), ((0j+-0.17,0j+0.29,0j+-0.66,0j+-0.62,0j+-0.97,0j+0.62,0j+0.58,0j+0.47,0j+-0.31,0j+0.33,0j+0.27,0j+-0.39,0j+0.19,0j+0.11,0j+-0.55,0j+-0.15,0j+0.42,0j+-0.17,0j+0.26,0j+-0.00,0j+-0.85,0j+0.86,0j+-0.46,0j+0.65,0j+0.26,0j+0.62,0j+0.40,0j+-0.06,0j+0.12,0j+-0.45,0j+0.57,0j+-0.76,0j+-0.88,0j+0.68,0j+-0.47,0j+-0.89,0j+-0.75,0j+-0.86,0j+-0.42,0j+-0.72,0j+-0.48,0j+0.79,0j+0.85,0j+-0.07,0j+0.57,0j+-0.87,0j+0.85,0j+-0.88,0j+0.85,0j+0.42,0j+-0.84,0j+0.82,0j+-0.26,0j+0.63,0j+-0.28,0j+-0.95,0j+0.81,0j+0.44,0j+0.77,0j+-0.60,0j+-0.07,0j+0.11,0j+0.53,0j+-0.84,0j+0.04,0j+-0.87,0j+-0.23,0j+0.38,0j+0.96,0j+0.45,0j+-0.43,0j+0.38,0j+0.21,0j+0.91,0j+-0.08,0j+-0.24,0j+0.85,0j+0.51,0j+0.52,0j+-0.96,0j+0.90,0j+0.04,0j+-0.28,0j+0.18,0j+-0.76,0j+0.38,0j+-0.02,0j+0.60,0j+-0.34,0j+-0.82,0j+0.98,0j+-0.33,0j+-0.55,0j+-0.41,0j+-0.62,0j+0.51,0j+0.04,0j+-0.08,0j+0.65,0j+0.17,0j+0.66,0j+-0.74,0j+-0.62,0j+-0.52,0j+0.81,0j+-0.13,0j+0.67,0j+0.85,0j+-0.77,0j+-0.27,0j+0.73,0j+1.00,0j+-0.81,0j+-0.90,0j+-0.59,0j+0.90,0j+-0.29,0j+0.33,0j+0.34,0j+0.52,0j+-0.61,0j+-0.37,0j+-0.27,0j+-0.35,0j+0.86,0j+0.23,0j+-0.05,0j+0.48),), "packet_len", True)
-        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((0j+1,1j+0,0j-1,0-1j), 9)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, 20,True)
-        self.blocks_tag_debug_0_0 = blocks.tag_debug(gr.sizeof_gr_complex*fft_len, '', "")
-        self.blocks_tag_debug_0_0.set_display(True)
-        self.blocks_tag_debug_0 = blocks.tag_debug(gr.sizeof_gr_complex*1, '', "")
-        self.blocks_tag_debug_0.set_display(True)
-        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 10, "packet_len")
-        self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 3, 100))), False)
+        self.qtgui_time_sink_x_0 = qtgui.time_sink_f(
+            2048, #size
+            24000, #samp_rate
+            "", #name
+            1, #number of inputs
+            None # parent
+        )
+        self.qtgui_time_sink_x_0.set_update_time(0.10)
+        self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+
+        self.qtgui_time_sink_x_0.set_y_label('Y', "")
+
+        self.qtgui_time_sink_x_0.enable_tags(True)
+        self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(True)
+        self.qtgui_time_sink_x_0.enable_grid(True)
+        self.qtgui_time_sink_x_0.enable_axis_labels(True)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        self.qtgui_time_sink_x_0.enable_stem_plot(False)
+
+
+        labels = ['Signal 1', 'Signal 2', 'Signal 3', 'Signal 4', 'Signal 5',
+            'Signal 6', 'Signal 7', 'Signal 8', 'Signal 9', 'Signal 10']
+        widths = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        colors = ['blue', 'red', 'green', 'black', 'cyan',
+            'magenta', 'yellow', 'dark red', 'dark green', 'dark blue']
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, 1.0]
+        styles = [1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1]
+
+
+        for i in range(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+
+        self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.qwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
+        self.mmse_resampler_xx_0 = filter.mmse_resampler_ff(0.0, samp_rate/48000)
+        self.fft_vxx_0 = fft.fft_vcc(fft_len, False, (), False, 1)
+        self.digital_ofdm_cyclic_prefixer_0 = digital.ofdm_cyclic_prefixer(
+            fft_len,
+            fft_len + fft_len//4,
+            4,
+            '')
+        self.digital_ofdm_carrier_allocator_cvc_0 = digital.ofdm_carrier_allocator_cvc( fft_len, [(4,5,6,7,9,10,11,12,14,15),(16,5,6,7,9,10,11,12,14,15)], [(3,8,13),(3,8,13)], [(1+0j,1+0j,1+0j),(1+0j,1+0j,1+0.2j)], ((0j+0.00,0j+0.00,0j+0.00,0j+0.12,0j+0.89,0j+-1.00,0j+0.24,0j+-0.64,0j+0.84,0j+-0.96,0j+-0.27,0j+-0.16,0j+-0.28,0j+-0.08,0j+0.27,0j+-0.50,0j+0.95,0j+0.82,0j+0.82,0j+0.53,0j+0.57,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00,0j+0.00),), "packet_len", False)
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((0j+1,1j+0,0j-1,0-1j), 1)
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink(
+            '/tmp/muell.wav',
+            1,
+            48000,
+            blocks.FORMAT_WAV,
+            blocks.FORMAT_PCM_16,
+            False
+            )
+        self.blocks_throttle_0_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, 20, "packet_len")
+        self.blocks_null_sink_0 = blocks.null_sink(gr.sizeof_float*1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_ff(0.075)
+        self.blocks_complex_to_float_0 = blocks.complex_to_float(1)
+        self.analog_random_source_x_0 = blocks.vector_source_b(list(map(int, numpy.random.randint(0, 3, 2500))), False)
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.analog_random_source_x_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
+        self.connect((self.blocks_complex_to_float_0, 0), (self.blocks_throttle_0_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_wavfile_sink_0, 0))
         self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_tag_debug_0, 0))
+        self.connect((self.blocks_throttle_0_0, 0), (self.blocks_null_sink_0, 0))
+        self.connect((self.blocks_throttle_0_0, 0), (self.mmse_resampler_xx_0, 0))
+        self.connect((self.blocks_throttle_0_0, 0), (self.qtgui_time_sink_x_0, 0))
         self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.digital_ofdm_carrier_allocator_cvc_0, 0))
-        self.connect((self.digital_ofdm_carrier_allocator_cvc_0, 0), (self.blocks_tag_debug_0_0, 0))
+        self.connect((self.digital_ofdm_carrier_allocator_cvc_0, 0), (self.fft_vxx_0, 0))
+        self.connect((self.digital_ofdm_cyclic_prefixer_0, 0), (self.blocks_complex_to_float_0, 0))
+        self.connect((self.fft_vxx_0, 0), (self.digital_ofdm_cyclic_prefixer_0, 0))
+        self.connect((self.mmse_resampler_xx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
 
 
     def closeEvent(self, event):
@@ -115,6 +187,8 @@ class ofdm_enc2(gr.top_block, Qt.QWidget):
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
+        self.blocks_throttle_0_0.set_sample_rate(self.samp_rate)
+        self.mmse_resampler_xx_0.set_resamp_ratio(self.samp_rate/48000)
 
     def get_fft_len(self):
         return self.fft_len
